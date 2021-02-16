5553f7650c2f8dea382f1cf1072a09c6434ecbca
Tjolanda Sullivan <tjolanda.sullivan@gmail.com>
2021-02-16 13:17:50 -0500

    basic layout for the main page and file cleanup

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <h1>This will be the HEADER</h1>
+      </header>
+      <body>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Welcome to the start of the temporarily named Body App
         </p>
-        <a
+      </body>
+       <footer className="App-footer">
+         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
@@ -17,7 +20,9 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+       </footer> 
+        
+      
     </div>
   );
 }
