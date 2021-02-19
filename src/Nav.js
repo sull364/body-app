ac619f43f757b55251887bf26b340acb8743f4ed
Tjolanda Sullivan <tjolanda.sullivan@gmail.com>
2021-02-19 14:32:47 -0500

    added burger functionality for small window

--- src/Nav.js
+++ src/Nav.js
@@ -1,6 +1,17 @@
 import "./App.scss";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
+
 function Nav() {
+  const [transformSetting, setTransformSetting] = useState({});
+
+  const navSlide = () => {
+    console.log("helloooo"); // delete later
+    if (transformSetting.transform === undefined)
+      return setTransformSetting({ transform: "translateX(0%)" });
+    else setTransformSetting({});
+  };
+
   return (
     <div className="Nav">
       <nav>
@@ -8,7 +19,7 @@ function Nav() {
           <h1>HEADER</h1>
         </div>
 
-        <ul className="nav-links">
+        <ul className="nav-links" style={transformSetting}>
           <Link to="/">
             <li>Home </li>
           </Link>
@@ -20,7 +31,7 @@ function Nav() {
           </Link>
         </ul>
 
-        <div className="burger">
+        <div className="burger" onClick={navSlide}>
           <div className="line1"></div>
           <div className="line2"></div>
           <div className="line3"></div>
